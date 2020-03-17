2eedf5c6f155cda35ff35a7d24d7419f27e4189f
Ivan5 <converse_ivan@hotmail.com>
2020-03-17 14:33:30 -0600

    Make a page to add a new article

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const articleRouter = require("./routes/articles");
 const app = express();
 
@@ -7,7 +8,19 @@ app.set("view engine", "ejs");
 app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const articles = [
+    {
+      title: "Test Article",
+      createdAt: new Date(),
+      description: "Test Description"
+    },
+    {
+      title: "Test Article 2",
+      createdAt: new Date(),
+      description: "Test Description 2"
+    }
+  ];
+  res.render("articles/index", { articles });
 });
 
 app.listen(5000);
